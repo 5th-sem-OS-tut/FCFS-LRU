4c8f8c88879a29be2580c09d3b18dcb11b0b4485
Shyam106 <96416370+Shyam106@users.noreply.github.com>
2022-01-02 20:44:07 +0530

    Included Hit and Miss ratios into the code

--- LRU.cpp
+++ LRU.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int nopages, nofaults, page[20],i ,count=0;
     cout<<"Enter the Number of pages :\t";
-    cin>> nopages;  
+    cin>> nopages;  //it will store the number of Pages
     cout<<"\nEnter the Reference String :\t";
     for(i=0;i< nopages;i++)
     {
@@ -17,7 +17,7 @@ int main()
     for(i=0;i< nofaults;i++)
     {
         frame[i]=-1;
-        fcount[i]=0; 
+        fcount[i]=0; //it will keep the track of when the page was last used
     }
     i=0;
     while(i< nopages)
@@ -25,7 +25,7 @@ int main()
         int j=0,flag=0;
         while(j< nofaults)
         {
-            if(page[i]==frame[j])  
+            if(page[i]==frame[j])  //it will check whether the page already exist in frames or not
             {
                 flag=1;
                 fcount[j]=i+1;
@@ -40,13 +40,13 @@ int main()
             int min=0,k=0;
             while(k<nofaults-1)
             {
-                if(fcount[min]>fcount[k+1]) 
+                if(fcount[min]>fcount[k+1]) //It will calculate the page which is least recently used
                 min=k+1;
                 k++;
             }
             frame[min]=page[i];
-            fcount[min]=i+1;  
-            count++;          
+            fcount[min]=i+1;  //Increasing the time
+            count++;          //it will count the total Page Fault
             while(j< nofaults)
             {
                 cout<<"\t|"<<frame[j]<<"|";
@@ -57,7 +57,10 @@ int main()
     }
     cout<<"\n\nNumber of Page Faults :"<<count;
     cout<<"\n\nNumber of Hits :"<<nopages - count;
-    cout<<"\n";
+    cout<<"\n" << endl;
+    cout << "Hit Ratio: " << (nopages-count)*100/nopages << "%" << endl;
+    cout << "Miss Ratio: " << count/nopages*100 << "%" << endl;
+
     return 0;
  }
 /*
